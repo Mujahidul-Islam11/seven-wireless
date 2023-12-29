6a722ec13e413d1ffc622d62f0b288fa875ffe10
Mohammad Dinar <web.dev.dinar@gmail.com>
2023-12-30 00:10:04 +0600

    Drawer ui added to the navbar

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,6 +1,16 @@
 import { HiBars3BottomLeft } from "react-icons/hi2";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Navbar = () => {
+  const links =(
+    <>
+    <h3 className="text-2xl md:text-3xl text-black cursor-pointer hover:text-[#405ED2] mb-4 md:mb-8">Home</h3>
+    <h3 className="text-2xl md:text-3xl text-black cursor-pointer hover:text-[#405ED2] mb-4 md:mb-8">About Us</h3>
+    <h3 className="text-2xl md:text-3xl text-black cursor-pointer hover:text-[#405ED2] mb-4 md:mb-8">Shop</h3>
+    <h3 className="text-2xl md:text-3xl text-black cursor-pointer hover:text-[#405ED2] mb-4 md:mb-8">News</h3>
+    <h3 className="text-2xl md:text-3xl text-black cursor-pointer hover:text-[#405ED2] mb-4 md:mb-8">Contact</h3>
+    </>
+  )
   return (
     <div className="bg-black fixed bg-opacity-20 shadow-xl z-10 py-4 w-full">
       <div className="flex justify-between md:w-[1300px] mx-auto">
@@ -16,7 +26,51 @@ const Navbar = () => {
         {/* Nav routes here */}
         <div>
           <nav>
-            <HiBars3BottomLeft className="text-5xl text-white"></HiBars3BottomLeft>
+            
+            <div className="drawer drawer-end">
+              <input
+                id="my-drawer-4"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content">
+                {/* Page content here */}
+                <label
+                htmlFor="my-drawer-4"
+                  className="drawer-button"
+                >
+                <HiBars3BottomLeft  className="text-5xl cursor-pointer text-white">
+                </HiBars3BottomLeft>
+                </label>
+              </div>
+              <div className="drawer-side">
+                <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                <ul className="menu p-4 pl-6 md:pl-16 w-80 md:w-[500px] min-h-full bg-white text-base-content">
+                  {/* Sidebar content here */}
+                  <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                >
+                  <div className="flex justify-end pr-6">
+                    <h3 className="text-2xl border-2 hover:border-gray-400 block px-2 rounded-full cursor-pointer text-gray-300 hover:text-gray-400">X</h3>
+                  </div>
+                </label>
+                    <div className=" mt-4 md:mt-8">
+                    {links}
+                    </div>
+                    <div className="flex gap-6 mt-10 md:mt-40">
+                      <FaFacebook className="text-3xl text-black hover:text-[#405DE2] cursor-pointer "></FaFacebook>
+                      <FaInstagram className="text-3xl text-black hover:text-[#405DE2] cursor-pointer "></FaInstagram>
+                      <FaYoutube className="text-3xl text-black hover:text-[#405DE2] cursor-pointer "></FaYoutube>
+                    </div>
+                </ul>
+              </div>
+            </div>
           </nav>
         </div>
       </div>
